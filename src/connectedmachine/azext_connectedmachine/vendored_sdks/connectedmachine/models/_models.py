f8ff90453682b1ef5ff2df46141244b3e385c9fd
Rohit <rjaini@microsoft.com>
2022-01-24 14:57:42 -0800

    Move out System managed identity (#1)

--- src/connectedmachine/azext_connectedmachine/vendored_sdks/connectedmachine/models/_models.py
+++ src/connectedmachine/azext_connectedmachine/vendored_sdks/connectedmachine/models/_models.py
@@ -357,7 +357,7 @@ class Identity(msrest.serialization.Model):
         'type': {'key': 'type', 'type': 'str'},
     }
 
-    type = "SystemAssigned"
+    type = "None"
 
     def __init__(
         self,
